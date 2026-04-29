62caa04ca09a1423901aa0a859b3a16f65a310de
Andrea Porcarelli <poandrea86@gmail.com>
2026-04-29 21:27:39 +0200

    Step 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Livewire\Livewire::setUpdateRoute(fn ($handle) => Route::post('/lw-update', $handle));
+        \Livewire\Livewire::setScriptRoute(fn ($handle) => Route::get('/lw-script.js', $handle));
     }
 }
