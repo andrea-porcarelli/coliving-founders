b3f943dfb2f998aed7927e6bb8ceb09e47fa5c32
Andrea Porcarelli <poandrea86@gmail.com>
2026-04-29 21:22:38 +0200

    Step 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,24 @@
 import './bootstrap';
-import Alpine from 'alpinejs';
 import Sortable from 'sortablejs';
 
-window.Alpine = Alpine;
 window.Sortable = Sortable;
 
-Alpine.directive('sortable', (el, { expression }, { evaluateLater, cleanup }) => {
-    const onEnd = evaluateLater(expression);
+document.addEventListener('alpine:init', () => {
+    window.Alpine.directive('sortable', (el, { expression }, { evaluateLater, cleanup }) => {
+        const onEnd = evaluateLater(expression);
 
-    const instance = Sortable.create(el, {
-        handle: '[data-drag-handle]',
-        animation: 150,
-        ghostClass: 'opacity-40',
-        onEnd: () => {
-            const ids = Array.from(el.querySelectorAll('[data-section-id]'))
-                .map(node => parseInt(node.dataset.sectionId, 10))
-                .filter(Boolean);
-            onEnd(() => {}, { scope: { $ids: ids } });
-        },
+        const instance = Sortable.create(el, {
+            handle: '[data-drag-handle]',
+            animation: 150,
+            ghostClass: 'opacity-40',
+            onEnd: () => {
+                const ids = Array.from(el.querySelectorAll('[data-section-id]'))
+                    .map(node => parseInt(node.dataset.sectionId, 10))
+                    .filter(Boolean);
+                onEnd(() => {}, { scope: { $ids: ids } });
+            },
+        });
+
+        cleanup(() => instance.destroy());
     });
-
-    cleanup(() => instance.destroy());
 });
-
-Alpine.start();
